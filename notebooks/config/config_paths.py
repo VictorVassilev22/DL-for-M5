8cf6f1619b2839abe95fd5bb37947e2725853328
VictorVassilev22 <victor.dimitrov.vasilev@gmail.com>
2023-08-05 20:35:04 +0300

    commit project

---
+++ notebooks/config/config_paths.py
@@ -0,0 +1,6 @@
+calendar_path = "../dataset/calendar.csv"
+sales_train_eval_path = "../dataset/sales_train_evaluation.csv"
+sales_train_val_path = "../dataset/sales_train_validation.csv"
+sample_submission_path = "../dataset/sample_submission.csv"
+sell_prices_path = "../dataset/sell_prices.csv"
+
